bd102e6c254f9acddeb5d44c8195c0e1cb812b8f
codesterAlex <laalit.sunaar@gmail.com>
2018-10-10 20:13:32 +0545

    Server/Test

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
+app.post('/todos',(req, res)=>{
   var todo = new Todo({
     text:req.body.text
   });
@@ -26,3 +26,5 @@ app.post('/todos',(req,res)=>{
 app.listen(3000,()=>{
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
